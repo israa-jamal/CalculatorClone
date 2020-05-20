16f5342492a4a4af96472c411e0d830f041f5687
Esraa Gamal <esraa.gamal98@gmail.com>
2020-05-21 01:24:24 +0200

    Added simple actions to buttons

--- CalculatorClone/Controller/ViewController.swift
+++ CalculatorClone/Controller/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
-
+    // var value = ""
+    private var finishedTyping : Bool = true
+    @IBOutlet weak var displayLabel: UILabel!
+    
     @IBOutlet weak var ZeroButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-}
+        
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        
+        if finishedTyping{
+            displayLabel.text = number
+            finishedTyping = false
+        }else{
+            if sender.currentTitle == "." && displayLabel.text?.firstIndex(of: ".") != nil {
+                return
+            }
+            displayLabel.text = displayLabel.text! + number
+        }
+        //value += number
+        
+        
+    }
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        finishedTyping = true
+        var num = displayLabel.text!
+        //var num = value
+        //value = ""
+        if let method = sender.currentTitle{
+            if method == "+/-" {
+                if num[num.startIndex] == "-" {
+                    num.remove(at: num.startIndex)
+                    displayLabel.text = num
+                }else{
+                    num.insert("-", at: num.startIndex)
+                    displayLabel.text = num
+                }
+            }
+            if method == "AC"
+            {
+                num = "0"
+                displayLabel.text = num
+            }
+            if method == "%"{
+                let value = Double(displayLabel.text!)!
+                displayLabel.text = String(value * 0.01)
+                num = displayLabel.text!
+                
+                
+            }
+        }
+    }
 }
